83364694bb87aec2072645a54b5934044ffd9ebe
Stefan Shepko-Hamilton <stefanshepko@gmail.com>
2020-02-29 16:40:19 -0500

    Authentication working.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+export const authEndpoint = 'https://accounts.spotify.com/authorize';
+var Spotify = require('spotify-web-api-js');
+// Replace with your app's client ID, redirect URI and desired scopes
+const clientId = ;
+const redirectUri = "http://localhost:3000/";
+const scopes = [
+  "user-read-currently-playing",
+  "user-read-playback-state",
+];
 
-function App() {
+var spotifyApi = new Spotify();
+
+// Get the hash of the url
+const hash = window.location.hash
+  .substring(1)
+  .split("&")
+  .reduce(function(initial, item) {
+    if (item) {
+      var parts = item.split("=");
+      initial[parts[0]] = decodeURIComponent(parts[1]);
+    }
+    return initial;
+  }, {});
+window.location.hash = "";
+class App extends Component {
+  componentDidMount() {
+    // Set token
+    let _token = hash.access_token;
+    if (_token) {
+      // Set token
+      this.setState({
+        token: _token
+      });
+    }
+  }
+render() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+      <img src={logo} className="App-logo" alt="logo" />
+      {!this.state ? (
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          className="btn btn--loginApp-link"
+          href={`${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join("%20")}&response_type=token&show_dialog=true`}
         >
-          Learn React
+          Login to Spotify{console.log(this)}
         </a>
+      ): (
+        spotifyApi.setAccessToken(this.state.token),
+        spotifyApi.getArtistAlbums('43ZHCT0cAZBISjO8DG9PnE', function(err, data) {
+          if (err) console.error(err);
+          else console.log('Artist albums', data);
+        }),
+        <p>test{`${this.state.token}`}</p>
+      )}
       </header>
     </div>
   );
+  }
 }
-
 export default App;
